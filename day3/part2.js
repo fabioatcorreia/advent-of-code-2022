6327492f3bb511d37a53d5b291b48a8520c7a258
Fábio Correia <fatcorreia94@gmail.com>
2022-12-05 18:38:16 +0000

    resolve day3 part2

---
+++ day3/part2.js
@@ -0,0 +1,94 @@
+const input = require("./input");
+
+const example = `vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`;
+
+const prorities = new Map([
+  ["a", 1],
+  ["b", 2],
+  ["c", 3],
+  ["d", 4],
+  ["e", 5],
+  ["f", 6],
+  ["g", 7],
+  ["h", 8],
+  ["i", 9],
+  ["j", 10],
+  ["k", 11],
+  ["l", 12],
+  ["m", 13],
+  ["n", 14],
+  ["o", 15],
+  ["p", 16],
+  ["q", 17],
+  ["r", 18],
+  ["s", 19],
+  ["t", 20],
+  ["u", 21],
+  ["v", 22],
+  ["w", 23],
+  ["x", 24],
+  ["y", 25],
+  ["z", 26],
+  ["A", 27],
+  ["B", 28],
+  ["C", 29],
+  ["D", 30],
+  ["E", 31],
+  ["F", 32],
+  ["G", 33],
+  ["H", 34],
+  ["I", 35],
+  ["J", 36],
+  ["K", 37],
+  ["L", 38],
+  ["M", 39],
+  ["N", 40],
+  ["O", 41],
+  ["P", 42],
+  ["Q", 43],
+  ["R", 44],
+  ["S", 45],
+  ["T", 46],
+  ["U", 47],
+  ["V", 48],
+  ["W", 49],
+  ["X", 50],
+  ["Y", 51],
+  ["Z", 52],
+]);
+
+const rucksacks = input.split("\n");
+
+function intersection(listA, listB) {
+  const setB = new Set(listB);
+  return [...listA].filter((item) => setB.has(item));
+}
+
+function getSumFromGroups() {
+  const groupsTotal = rucksacks.length / 3;
+
+  const groupsItemPriorities = [];
+
+  for (let index = 0; index < groupsTotal; index++) {
+    const groupIndex = index * 3;
+    const elf1 = rucksacks[groupIndex];
+    const elf2 = rucksacks[groupIndex + 1];
+    const elf3 = rucksacks[groupIndex + 2];
+
+    const groupItemType = intersection(intersection(elf1, elf2), elf3)[0];
+
+    groupsItemPriorities.push(prorities.get(groupItemType));
+  }
+
+  return groupsItemPriorities.reduce(
+    (currentSum, groupItemPriority) => currentSum + groupItemPriority,
+    0
+  );
+}
+
+console.log( getSumFromGroups());
