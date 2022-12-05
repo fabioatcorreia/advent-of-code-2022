02aa85374057864cf3ed1b56cf184c3299ee4549
Fábio Correia <fatcorreia94@gmail.com>
2022-12-05 15:23:27 +0000

    resolve day 1 part 2

---
+++ day1/part2.js
@@ -0,0 +1,13 @@
+const input = require('./part1-input');
+const itemsByElf = input.split('\n\n').map(elfFood => elfFood.split('\n'));
+
+function sum(a, b) {
+    return Number(a) + Number(b);
+}
+
+const caloriesByElf = itemsByElf.map((elfItems) => elfItems.reduce(sum, 0))
+caloriesByElf.sort((a, b) => b - a);
+
+const result = caloriesByElf.slice(0, 3).reduce(sum, 0);
+
+console.log(result);
