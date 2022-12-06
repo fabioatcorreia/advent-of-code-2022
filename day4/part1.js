228649a2533fb7a01aa55c71f115127ff0d1693c
Fábio Correia <fatcorreia94@gmail.com>
2022-12-06 15:01:11 +0000

    resolve day4 part1

---
+++ day4/part1.js
@@ -0,0 +1,29 @@
+const input = require("./input");
+
+const example = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`;
+
+const assignmentsPairs = input.split("\n");
+
+function areAssignmentsIntersecting(assignment1, assignment2) {
+    const assignment1Range = assignment1.split("-").map(Number);
+    const assignment2Range = assignment2.split("-").map(Number);
+
+    return assignment1Range[0] >= assignment2Range[0] && assignment1Range[1] <= assignment2Range[1]
+    || assignment1Range[0] <= assignment2Range[0] && assignment1Range[1] >= assignment2Range[1]
+}
+
+const numberOfAssignmentsContainingItsPair = assignmentsPairs.filter(
+    (assignmentPair) => {
+        const assignments = assignmentPair.split(',');
+        
+        return areAssignmentsIntersecting(assignments[0], assignments[1])
+    }
+)
+
+
+console.log(numberOfAssignmentsContainingItsPair.length);
